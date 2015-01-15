b7027ab3b98c45e03c52724f28027189a53ed461
Grupen <thatpenguin99661@gmail.com>
2015-01-15 22:42:39 +0100

    commented

--- TableCalculator.java
+++ TableCalculator.java
@@ -14,8 +14,9 @@ public class TableCalculator {
         int value1=input.nextInt();
         System.out.println("Please enter how high it will go: ");
         long value2=input.nextInt();
-        
+        // for loop explained for(declare long i and set value to 0: as long as i<value2, the program will continue: add whatever the user specified)
         for(long i=0; i<value2; i=i+value1){
+
             System.out.printf("%d\n",i);
         }
     }
